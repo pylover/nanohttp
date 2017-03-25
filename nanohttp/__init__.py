5853b080589be251426c6401cbb9e02513ccadf7
Vahid <vahid.mardani@gmail.com>
2017-03-26 00:52:25 +0430

    v0.2.0

--- nanohttp/__init__.py
+++ nanohttp/__init__.py
@@ -6,9 +6,9 @@ from .exceptions import HttpStatus, HttpBadRequest, HttpUnauthorized, HttpForbid
 from .cookies import HttpCookie
 from .controllers import Controller, RestController, Static
 from .decorators import action, html, json, xml, binary, text
-from .helpers import quickstart
+from .helpers import quickstart, LazyAttribute
 from .cli import main
 from .contexts import context, ContextIsNotInitializedError
 from .configuration import settings, configure
 
-__version__ = '0.2.0-dev'
+__version__ = '0.2.0'
