3a77585b5ac047ce091496ab96fa6157801f3c54
pylover <vahid.mardani@gmail.com>
2017-04-19 20:56:52 +0430

    Preparing for release 0.3.0

--- nanohttp/__init__.py
+++ nanohttp/__init__.py
@@ -11,4 +11,4 @@ from .cli import main
 from .contexts import context, ContextIsNotInitializedError
 from .configuration import settings, configure
 
-__version__ = '0.3.0-dev'
+__version__ = '0.3.0'
