f86befdd7bbb7ab51f532148c273ac712c2ce965
Vahid <vahid.mardani@gmail.com>
2017-06-02 22:07:11 +0430

    v0.9.1

--- nanohttp/__init__.py
+++ nanohttp/__init__.py
@@ -12,4 +12,4 @@ from .contexts import context, ContextIsNotInitializedError
 from .configuration import settings, configure
 from .application import Application
 
-__version__ = '0.9.0'
+__version__ = '0.9.1'
