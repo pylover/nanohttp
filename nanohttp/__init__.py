5475fe73a4102549344f879023dcac5ca6549ec1
pylover <vahid.mardani@gmail.com>
2017-06-07 03:08:31 +0430

    Changing docs theme

--- nanohttp/__init__.py
+++ nanohttp/__init__.py
@@ -12,4 +12,4 @@ from .contexts import context, ContextIsNotInitializedError
 from .configuration import settings, configure
 from .application import Application
 
-__version__ = '0.9.6'
+__version__ = '0.9.7'
