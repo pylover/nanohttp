9d565c35cacf1caabe4f3fdf16e8ee556f66c1ac
pylover <vahid@carrene.com>
2017-06-14 17:04:20 +0430

    Nothing

--- practice/type_annotation.py
+++ practice/type_annotation.py
@@ -11,18 +11,24 @@ def w(*args, **kwargs):
     return a(*args, **kwargs)
 
 
+def ann(a: int):
+    pass
+
+
 def describe(f):
     print(f.__name__)
-    code = f.__code__
-    for k in dir(code):
-        if k.startswith('co_'):
-            v = getattr(code, k)
-            print(k, v)
+    # code = f.__code__
+    # for k in dir(code):
+    #     if k.startswith('co_'):
+    #         v = getattr(code, k)
+    #         print(k, v)
+    print('__annotations__', f.__annotations__)
 
 
 def main():
     describe(a)
     describe(w)
+    describe(ann)
 
 
 if __name__ == '__main__':
