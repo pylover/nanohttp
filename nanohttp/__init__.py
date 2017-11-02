31b905d464a8d69b5356739c2f5601d4bbbad4cd
pylover <vahid@carrene.com>
2017-11-02 11:12:22 +0330

    v0.20.1

--- nanohttp/__init__.py
+++ nanohttp/__init__.py
@@ -11,4 +11,4 @@ from .contexts import context, ContextIsNotInitializedError
 from .configuration import settings, configure
 from .application import Application
 
-__version__ = '0.20.0'
+__version__ = '0.20.1'
