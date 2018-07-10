065a13db3c1fba0ef037c8923c0e706496efba50
mehrdad1373pedramfar <mehrdad1373pedramfar@yahoo.com>
2018-07-10 15:09:38 +0430

    Fix code styling for nanohttp/tests/test_main.py, #70

--- nanohttp/tests/test_main.py
+++ nanohttp/tests/test_main.py
@@ -23,7 +23,14 @@ class EntryPointTestCase(unittest.TestCase):
 
     def test_main_function(self):
 
-        args = ['nanohttp', '-C', self.module_dir, '-b', str(self.port), ':Static']
+        args = [
+            'nanohttp',
+            '-C',
+            self.module_dir,
+            '-b',
+            str(self.port),
+            ':Static'
+        ]
         t = threading.Thread(target=main, args=(args,), daemon=True)
         t.start()
 
