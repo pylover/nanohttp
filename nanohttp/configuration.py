610117fa55c2bcbd65f8db6d57ca48828ac7b99d
mehrdad1373pedramfar <mehrdad1373pedramfar@yahoo.com>
2018-07-10 15:22:58 +0430

    Fix code styling for nanohttp/configuration.py, #70

--- nanohttp/configuration.py
+++ nanohttp/configuration.py
@@ -15,11 +15,14 @@ cookie:
 
 
 def configure(*args, **kwargs):
-    """ Load configurations
+    """Load configurations
 
-    .. seealso:: `pymlconf Documentations <https://github.com/pylover/pymlconf#documentation>`_
+    .. seealso:: `pymlconf Documentations
+    <https://github.com/pylover/pymlconf#documentation>`_
 
-    :param args: positional arguments pass into ``pymlconf.DeferredConfigManager.load``
-    :param kwargs: keyword arguments pass into ``pymlconf.DeferredConfigManager.load``
+    :param args: positional arguments pass into
+     ``pymlconf.DeferredConfigManager.load``
+    :param kwargs: keyword arguments pass into
+    ``pymlconf.DeferredConfigManager.load``
     """
     settings.load(*args, builtin=BUILTIN_CONFIGURATION, **kwargs)
