946fe78708c4e832aab8891f0f51e6de1760791f
mehrdad1373pedramfar <mehrdad1373pedramfar@yahoo.com>
2018-07-10 15:08:37 +0430

    Fix code styling for nanohttp/tests/test_forms.py, #70

--- nanohttp/tests/test_forms.py
+++ nanohttp/tests/test_forms.py
@@ -14,11 +14,15 @@ class FormTestCase(WsgiAppTestCase):
         def index(self):
             if not context.form:
                 yield 'empty'
-            yield ', '.join('%s: %s' % (k, v) for k, v in sorted(context.form.items(), key=lambda x: x[0]))
+            yield ', '.join('%s: %s' % (k, v)
+                for k, v in sorted(context.form.items(), key=lambda x: x[0]))
 
     def test_simple_query_string(self):
-        self.assert_post('/?a=1&b=&c=2', expected_response="a: 1, b: , c: 2")
-        self.assert_post('/?a=1&b=2&b=3', expected_response="a: 1, b: ['2', '3']")
+        self.assert_post('/?a=1&b=&c=2', expected_response='a: 1, b: , c: 2')
+        self.assert_post(
+            '/?a=1&b=2&b=3',
+            expected_response='a: 1, b: [\'2\', \'3\']'
+        )
 
     def test_url_encoded(self):
         self.assert_post(
@@ -38,7 +42,10 @@ class FormTestCase(WsgiAppTestCase):
                 'b': [2, 3],
             },
             files={'c': join(STUFF_DIR, 'cat.jpg')},
-            expected_response=re.compile("a: 1, b: \[2, 3\], c: FieldStorage\('c', 'cat\.jpg.*"),
+            expected_response=\
+                re.compile(
+                    'a: 1, b: \[2, 3\], c: FieldStorage\(\'c\', \'cat\.jpg.*'
+                ),
         )
 
     def test_empty_form(self):
