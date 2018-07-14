42ef4c7dc1905018a6457c6d5259ea7776e9601e
pylover <vahid.mardani@gmail.com>
2018-07-14 18:51:47 +0000

    Deploy to GitHub Pages: 234125749656705636e16561f5f14c0534c70d53

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.2a1',
+    VERSION: '1.0.3a1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
