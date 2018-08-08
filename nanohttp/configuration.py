c7fcd56694dca15ebe318f58a5283ea6c5d492f9
pylover <vahid.mardani@gmail.com>
2018-08-08 21:34:11 +0430

    TODOs and FIXMEs #77

--- nanohttp/configuration.py
+++ nanohttp/configuration.py
@@ -18,11 +18,11 @@ def configure(*args, **kwargs):
     """Load configurations
 
     .. seealso:: `pymlconf Documentations
-    <https://github.com/pylover/pymlconf#documentation>`_
+                 <https://github.com/pylover/pymlconf#documentation>`_
 
     :param args: positional arguments pass into
-     ``pymlconf.DeferredConfigManager.load``
+                 ``pymlconf.DeferredConfigManager.load``
     :param kwargs: keyword arguments pass into
-    ``pymlconf.DeferredConfigManager.load``
+                   ``pymlconf.DeferredConfigManager.load``
     """
     settings.load(*args, builtin=BUILTIN_CONFIGURATION, **kwargs)
