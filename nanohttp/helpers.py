d1d76a83cd07bf387e57627de804405b0bd42f9a
pylover <vahid.mardani@gmail.com>
2018-08-08 20:04:58 +0430

    Providing __name__ and __doc__ for LazyAttributes wrapped function, closes #56

--- nanohttp/helpers.py
+++ nanohttp/helpers.py
@@ -34,8 +34,13 @@ class LazyAttribute(object):
     def __init__(self, f):
         self.f = f
 
+    def __doc__(self):
+        return self.f.__doc__
+
     def __get__(self, obj, t=None):
         f = self.f
+        if obj is None:
+            return f
         val = f(obj)
         setattr(obj, f.__name__, val)
         return val
