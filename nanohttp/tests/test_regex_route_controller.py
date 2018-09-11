b4523a9ea58c1c442abbe129a679291cbbace42d
pylover <vahid.mardani@gmail.com>
2018-09-12 01:03:00 +0430

    Testing Regex Route controller, #117

---
+++ nanohttp/tests/test_regex_route_controller.py
@@ -0,0 +1,34 @@
+from bddrest import status, response
+
+from nanohttp import RegexRouteController, action
+from nanohttp.tests.helpers import Given, when
+
+
+def test_rest_controller():
+    class Root(RegexRouteController):
+        def __init__(self):
+            super().__init__((
+                (r'/foos/(?P<id>\d+)/bars',
+                self.bars),
+            ))
+
+        @action
+        def bars(self, id: int):
+            return f'{id}'
+
+    with Given(Root(), '/foos/1/bars'):
+        assert status == 200
+        assert response.text == '1'
+
+        when('/foos')
+        assert status == 404
+
+        when('/foos/bars')
+        assert status == 404
+
+        when('/foos/1/2/bars')
+        assert status == 404
+
+        when('/foos/a/bars')
+        assert status == 404
+
