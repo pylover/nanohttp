02da004d9f43d971045dac02b8861dfb0a72bea6
pylover <vahid@carrene.com>
2018-09-15 16:32:11 +0430

    v1.9.2

--- nanohttp/__init__.py
+++ nanohttp/__init__.py
@@ -14,4 +14,4 @@ from .configuration import settings, configure
 from .application import Application
 from .validation import validate, RequestValidator
 
-__version__ = '1.9.1'
+__version__ = '1.9.2'
