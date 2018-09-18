a4b9d9aad660ce769a74dc814f6c2c022a7f3255
pylover <vahid.mardani@gmail.com>
2018-09-18 19:07:32 +0430

    Fighting the travis multiprocessing and change path bug

--- nanohttp/tests/test_cli.py
+++ nanohttp/tests/test_cli.py
@@ -10,35 +10,38 @@ def test_cli_without_arguments(clitool):
     assert len(response.text) > 1
 
 
-# Commented out because of the travis buf about change ditectory:
+# Erorred because of the travis buf about change ditectory:
 # https://github.com/pytest-dev/pytest/issues/2686
-#def test_cli_with_change_directory_flag(clitool):
-#    url = clitool.execute('-C', '/tmp')
-#    response = requests.get(url)
-#    assert response.status_code == 200
-#    assert len(response.text) > 1
-#def test_cli_with_given_module_name(clitool, controller_file):
-#    directory, filename = path.split(controller_file())
-#    # Removing the module[.py] extension by filename[:-3]
-#    url = clitool.execute('-C', directory, filename[:-3])
-#    response = requests.get(url)
-#    assert response.status_code == 200
-#    assert response.text == 'Index'
-#def test_cli_with_change_dir_and_controller_filename(clitool, controller_file):
-#    directory, filename = path.split(controller_file())
-#    url = clitool.execute('-C', directory, filename)
-#    response = requests.get(url)
-#    assert response.status_code == 200
-#    assert response.text == 'Index'
-#
-#
-#def test_cli_with_static_controller(clitool, make_temp_directory):
-#    directory = make_temp_directory(a='Hello A')
-#    url = clitool.execute('-C', directory, ':Static')
-#    response = requests.get(f'{url}/a')
-#    assert response.status_code == 200
-#    assert response.text == 'Hello A'
+def test_cli_with_change_directory_flag(clitool):
+    url = clitool.execute('-C', '/tmp')
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert len(response.text) > 1
 
+
+def test_cli_with_given_module_name(clitool, controller_file):
+    directory, filename = path.split(controller_file())
+    # Removing the module[.py] extension by filename[:-3]
+    url = clitool.execute('-C', directory, filename[:-3])
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert response.text == 'Index'
+
+
+def test_cli_with_change_dir_and_controller_filename(clitool, controller_file):
+    directory, filename = path.split(controller_file())
+    url = clitool.execute('-C', directory, filename)
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert response.text == 'Index'
+
+
+def test_cli_with_static_controller(clitool, make_temp_directory):
+    directory = make_temp_directory(a='Hello A')
+    url = clitool.execute('-C', directory, ':Static')
+    response = requests.get(f'{url}/a')
+    assert response.status_code == 200
+    assert response.text == 'Hello A'
 
 
 def test_cli_with_config_file_flag(clitool, make_temp_file):
