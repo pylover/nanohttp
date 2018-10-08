13e9b89843bc3129438b25b1dd1a5799ee4b289a
pylover <vahid.mardani@gmail.com>
2018-10-08 20:48:43 +0330

    v1.10.1

--- nanohttp/__init__.py
+++ nanohttp/__init__.py
@@ -14,4 +14,4 @@ from .configuration import settings, configure
 from .application import Application
 from .validation import validate, RequestValidator
 
-__version__ = '1.10.0'
+__version__ = '1.10.1'
