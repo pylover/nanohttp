667f22962f9e7ab1d695b2adb3cd1ccbc286fe2e
mohadese <mohadeseyusefi1997@gmail.com>
2019-03-07 13:06:48 +0330

    Not Null, required and min&max test

--- nanohttp/tests/test_HTTPSstatus_validation.py
+++ nanohttp/tests/test_HTTPSstatus_validation.py
@@ -1,12 +1,10 @@
 import re
-from decimal import Decimal
 
 import pytest
 from bddrest import status, response
 
 from nanohttp import RequestValidator, HTTPBadRequest, HTTPStatus, \
-    Controller, validate, action, context
-from nanohttp.tests.helpers import Given
+    Controller, validate
 
 
 def test_not_null():
@@ -36,7 +34,38 @@ def test_validation_required_as_HTTPstatus():
     with pytest.raises(HTTPStatus(Myclass).__class__):
         validator(dict(another_param='value1'))
 
-
     assert dict(a='value1') == validator(dict(a='value1'))[0]
 
+def test_httpstatus_as_validation_error():
+    class MyStatusMin(HTTPStatus):
+        status = '600 Greater than the minimum length'
+
+    class MyStatusMax(HTTPStatus):
+        status = '601 Greater than the maximum length'
+
+    class MyStatusPattern(HTTPStatus):
+        status = '602 Invalid input format'
+
+
+    validator = RequestValidator(
+        fields=dict(
+            a=dict(
+                min_length=(2, MyStatusMin),
+                max_length=(5, MyStatusMax),
+                pattern=(r'^[A-Z]*$', MyStatusPattern)
+            )
+        )
+    )
+
+    with pytest.raises(HTTPStatus) as ctx:
+        validator(dict(a='A'))
+        assert str(ctx.value) == MyStatusMin.status
+
+    with pytest.raises(HTTPStatus) as ctx:
+        validator(dict(a=(5 + 1) * 'A'))
+    assert str(ctx.value) == MyStatusMax.status
+
+    with pytest.raises(HTTPStatus) as ctx:
+        validator(dict(a='abc'))
+    assert str(ctx.value) == MyStatusPattern.status
 
